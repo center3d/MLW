ceff28f4164a234c2ebcbaf8f11f295c1f52b7c1
Peeyush pashine <peeyushpashine@gmail.com>
2020-02-05 15:48:54 +0530

    Update and rename ZMODMacOS.sh to mlwMacOS.sh

--- Docker/Mac/mlwMacOS.sh
+++ Docker/Mac/mlwMacOS.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 if [ -z "$2"];
 then
-	export hostdir=$(pwd)
+        export hostdir=$(pwd)
 else
 export hostdir=$2
 fi
 echo $hostdir
-echo 2nd argument $2
 set -a
-sed -i '' -e "/^hostdir=/d" ~/.zmod/.env 
-echo hostdir=$hostdir >> ~/.zmod/.env
-source ~/.zmod/.env
-echo after sourcing .sh file $hostdir
+sed -i '' -e "/^hostdir=/d" ~/.mlw/.env
+echo hostdir=$hostdir >> ~/.mlw/.env
+source ~/.mlw/.env
 if [[ $1 = "up" || $1 = "" || $1 = "." ]];
 then
-echo Starting ZMOD
+echo Starting mlw
 #cd $hostdir
 cat ${COMPOSE_CONFIG} | envsubst | sudo docker-compose -f - up -d
-docker exec -it nginx_zmod bash -c 'rm nginx/conf.d/default.conf; service nginx restart'
+docker exec -it nginx_mlw bash -c 'rm nginx/conf.d/default.conf; service nginx restart'
 docker exec -it zmm bash -c 'echo "  inside zmm" && echo alias nyoka="/publish/nyokacli/client/bin/release/netcoreapp2.1/linux-x64/publish/nyoka" >> ~/.bashrc && echo "DONE"'
-echo in $(pwd)
 
 
-echo ZMOD is up now
+echo mlw is up now
 else
-        echo Stopping ZMOD
-	sudo docker stop zmk1 zmk2 zmk3
+        echo Stopping mlw
+        sudo docker stop zmk1 zmk2 zmk3
         cat ${COMPOSE_CONFIG} | envsubst | sudo docker-compose -f - down
-        echo ZMOD is stopped now
+        echo mlw is stopped now
 fi
-
-
